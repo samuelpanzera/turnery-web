b11c9254139f50b08e6b93f4fbbe9bf89907483b
Samuel Panzera <samuelpanzera@gmail.com>
2025-04-03 23:02:24 -0300

    Caminhando para o Toggle funcionar, encontrei o erro!

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js (ou .mjs/.ts se preferir/configurado)
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [
+  ],
+}
