43e2698b249f628096a979cf695bccfc855b0a46
Samuel Panzera <samuelpanzera@gmail.com>
2025-04-14 21:25:29 -0300

    Atualização dos pacotes, criação do Header e definição da paleta de cores

--- src/components/headers/Header.tsx
+++ src/components/headers/Header.tsx
@@ -3,15 +3,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-amber-100 shadow justify-items-center flex justify-between">
+    <header className="bg-[#ffffff] shadow-2xl flex justify-between items-center px-10 py-4">
       <Image
         alt="Logotipo da empresa"
-        width={100}
+        width={220}
         height={30}
         src="/assets/logotipo.png"
       />
-
-      <nav className="gap-4 flex aling-items-center h-[8%] font-bold">
+      <nav className="flex gap-6 items-center font-bold [&>a]:relative [&>a]:after:absolute [&>a]:after:bg-black [&>a]:after:h-0.5 [&>a]:after:w-0 [&>a]:after:left-0 [&>a]:after:bottom-0 [&>a]:after:transition-all [&>a:hover]:after:w-full">
         <Link href="/">Home</Link>
         <Link href="/sobre-nos">Sobre nós</Link>
         <Link href="/fale-conosco">Fale Conosco</Link>
